b0de0a714017c48e12156a33e322bf2d33da2201
Manikandan-Lakshmanan <93248283+Manikandan-Lakshmanan@users.noreply.github.com>
2022-06-29 14:43:56 +0530

    documentation(EJ2-60985): sample added

---
+++ Angular/src/app/app.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit, ViewEncapsulation, ViewChild, } from '@angular/core';
+import {
+    ChartComponent,
+    IPointEventArgs,
+  } from '@syncfusion/ej2-angular-charts';
+  
+/**
+ * Sample for  Stacked water fall chart
+ */
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['app.component.css'],
+    encapsulation: ViewEncapsulation.None
+})
+export class AppComponent {
+
+    @ViewChild('chart')
+    public chart!: ChartComponent;
+    public data: Object[] = [
+      { x: '2020', y: 46 },
+      { x: '2021', y: 27 },
+    ];
+    public month: Object[] = [
+      { x: 'Jan', y: 46 },
+      { x: 'Feb', y: 27 },
+      { x: 'Mar', y: 40 },
+      { x: 'Apr', y: 20 },
+      { x: 'May', y: 36 },
+      { x: 'Jun', y: 17 },
+      { x: 'Jul', y: 26 },
+      { x: 'Aug', y: 37 },
+      { x: 'Sep', y: 46 },
+      { x: 'Oct', y: 17 },
+      { x: 'Nov', y: 16 },
+      { x: 'Dec', y: 37 },
+    ];
+    public week: Object[] = [
+      { x: 'Week 1', y: 46 },
+      { x: 'Week 2', y: 27 },
+      { x: 'Week 3', y: 40 },
+      { x: 'Week 4', y: 20 },
+      { x: 'Week 5', y: 36 },
+      { x: 'Week 6', y: 17 },
+    ];
+    //Initializing Primary X Axis
+    public primaryXAxis: Object = {
+      valueType: 'Category',
+      interval: 1,
+      width: 2,
+      majorGridLines: { width: 0 },
+    };
+    //Initializing Primary Y Axis
+    public primaryYAxis: Object = {
+      majorGridLines: { width: 0 },
+      majorTickLines: { width: 0 },
+      lineStyle: { width: 0 },
+      labelStyle: { color: 'transparent' },
+    };
+  
+    public title: string = 'Olympic Medal Counts - RIO';
+    public tooltip: Object = {
+      enable: true,
+    };
+  
+    public pointClick(args: IPointEventArgs): void {
+      if (args.point.x === '2020' || args.point.x === '2021') {
+        args.series.dataSource = this.month;
+      } else {
+        args.series.dataSource = this.week;
+      }
+      let button: HTMLElement = <HTMLElement>document.getElementById('button'); 
+      button.style.display = 'block';        
+      (<HTMLElement>document.getElementById('category')).style.visibility = 'visible';
+    
+    };
+    // custom code end
+    public chartArea: Object = {
+      border: {
+        width: 0,
+      }
+    };
+    public width: string = '40%';
+    ngAfterViewInit() {
+        (<HTMLElement>document.getElementById('category')).onclick = () => {
+            this.chart.series[0].dataSource = this.data;
+            let button: HTMLElement = <HTMLElement>document.getElementById('button');
+            button.style.display = 'none';
+            (<HTMLElement>document.getElementById('category')).style.visibility = 'hidden';
+        };
+    }
+  
+    constructor() {
+      //code
+    }
+
+}
